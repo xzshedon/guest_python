0f491f14b727bf88c1aa3dbd6422009ba309a747
xzshedon <568165769@qq.com>
2018-01-04 10:56:01 +0800

    Fix admin page view

--- guest/urls.py
+++ guest/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     url(r'^logout/$', views.logout),
     url(r'^sign_index/(?P<eid>[0-9]+)/$',views.sign_index),
     url(r'^sign_index_action/(?P<eid>[0-9]+)/$', views.sign_index_action),
-    url(r'^api/',include('sign.urls',namespace='sign')),
+    # url(r'^api/',include('sign.urls', namespace='sign')),
 ]
